357851b9e619d152d3ef7b4edd4ee91af7b3453c
tianye.xt <tianye.xt@bytedance.com>
2025-04-18 14:44:41 +0800

    chore: rm region block.

--- app/src/main/kotlin/dev/aaa1115910/bv/activities/MainActivity.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/activities/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.aaa1115910.bv.repository.UserRepository
 import dev.aaa1115910.bv.screen.MainScreen
-import dev.aaa1115910.bv.screen.RegionBlockScreen
 import dev.aaa1115910.bv.screen.user.lock.UnlockUserScreen
 import dev.aaa1115910.bv.ui.theme.BVTheme
 import dev.aaa1115910.bv.util.NetworkUtil
@@ -62,8 +61,6 @@ class MainActivity : ComponentActivity() {
             BVTheme {
                 if (isChecking) {
                     //避免在检查网络的期间加载屏幕内容，导致检查完毕后显示屏幕内容时出现初始焦点未成功设置的问题
-                } else if (isMainlandChina) {
-                    RegionBlockScreen()
                 } else {
                     //HomeScreen()
                     if (!userLockLocked) {
